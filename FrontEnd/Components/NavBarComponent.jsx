58e517f41655ca8ba8c5e8d84b7b9d815501f079
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-11 01:20:01 +0545

    Update NavBarComponent.jsx

--- FrontEnd/Components/NavBarComponent.jsx
+++ FrontEnd/Components/NavBarComponent.jsx
@@ -24,21 +24,20 @@ const NavBarComponent = () => {
     }, []);
 
   return (
-    <div className='flex flex-row justify-between pl-20 pr-20 bg-[#33cc33] items-center h-20 gap-8'>
-        <Link href={"./"}><div className='flex w-80 rounded-2xl justify-center pt-2 pb-2 items-center gap-4 cursor-pointer'>
+    <div className='flex flex-row justify-between bg-[#33cc33] items-center h-20 gap-8 w-screen'>
+        <Link href={"./"}>
+        <div className='flex w-80 rounded-2xl justify-center pl-16 pt-2 pb-2 items-center gap-4 cursor-pointer'>
           <Image src="/lgo.png" width={80} height={80} alt="Sed Pepe Logo"/>
           <h4 className=' text-3xl font-a text-white font-semibold'> SED PEPE</h4>
-        </div></Link>
+        </div>
+        </Link>
 
-        <div className='flex flex-row w-3/4 items-center justify-center'>
-
-      <div className='flex flex-row gap-4 text-white items-center text-xl font-bold font-a justify-center'>
-      <Link href ={'./'}><div className='flex rounded-xl p-2 hover:text-red-500 cursor-pointer hover:scale-110'>HOME</div></Link>
+      <div className='flex flex-row w-4/6 items-center justify-start gap-4 text-white text-xl font-bold font-a'>
+        <Link href ={'./'}><div className='flex rounded-xl p-2 hover:text-red-500 cursor-pointer hover:scale-110'>HOME</div></Link>
         <Link href ={'./litepaper'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>LITEPAPER</div></Link>
         <Link href ={'./'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>ROADMAP</div></Link>
         <Link href ={'./'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>TEAM</div></Link>
         <Link href ={'./profile'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>BUY $SPEPE</div></Link>
-      </div>
       </div>
       
     </div>
