b92c1c9f7b5103fb80da4342f10b76f3d6eb0149
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-11 04:06:16 +0545

    sad

--- FrontEnd/Components/Whitepaper.jsx
+++ FrontEnd/Components/Whitepaper.jsx
@@ -5,7 +5,7 @@ const Whitepaper = () => {
   return (
     <div className=' bg-[#2b892b] text-black w-screen h-screen '>
       <div className='flex justify-start absolute inset-y-22 w-screen h-screen inset-x-0' style={{ opacity: 0.18 }}>
-      <Image src="/bg.gif" layout="fill" objectFit="cover" />
+      <Image src="/sadd.gif" layout="fill" objectFit="cover" />
       </div>
       <div className='relative z-10'>
         WhitePaper
