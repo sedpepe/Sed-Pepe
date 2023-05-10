138a4be4bf6e7066c2fa115d9f77fbf2e2be9f4a
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-10 22:32:23 +0545

    homepage

--- FrontEnd/Components/HomePage.jsx
+++ FrontEnd/Components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useContext } from 'react'
 import { AppContext } from '../Api/contextApi';
 import Image from 'next/image';
+import Footer from './Footer';
 
 const HomePage = () => {
     const {connectedUser ,MintNFT } =useContext(AppContext);
@@ -28,9 +29,18 @@ const HomePage = () => {
         </div>
         </div>
 
-        <h2 className='font-semibold text-4xl pt-12 text-white underline-offset-8 underline'>Links:</h2>
-          <div>
+        <h2 className='font-semibold text-4xl pt-16 text-white underline-offset-8 underline'>Links:</h2>
+          <div className='flex pt-12 gap-8 items-center'>
+            <Image src='/twitter.png' width={100} height={100}/>
+            <Image src='/tg2.png' width={100} height={100}/>
+            <Image src='/uni.png' width={100} height={100}/>
+            <Image src='https://etherscan.io/images/brandassets/etherscan-logo-circle.svg' width={75} height={75} className='pt-4'/>
+          </div> 
 
+          <h4 className='font-semibold text-2xl pt-12 text-white pt-24'>Powered By :</h4>
+          <div className='flex gap-8 pt-2'>
+          <Image src='/thirdweb.svg' width={90} height={90}/>
+          <Image src='/Arbitrum.png' width={100} height={100}/>
           </div>
       </div>
     </div>
--- FrontEnd/pages/_app.js
+++ FrontEnd/pages/_app.js
@@ -4,6 +4,7 @@ import { AppProvider } from "../Api/contextApi";
 import NavBarComponent from "../Components/NavBarComponent";
 import Header from "../Components/Header";
 import "../styles/globals.css";
+import Footer from "../Components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
