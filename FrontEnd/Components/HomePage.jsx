85c826013df13f5470076a00e2505df4fbdd71f4
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-10 23:35:25 +0545

    gif bg

--- FrontEnd/Components/HomePage.jsx
+++ FrontEnd/Components/HomePage.jsx
@@ -8,8 +8,8 @@ const HomePage = () => {
     const {connectedUser ,MintNFT } =useContext(AppContext);
   return (
     <div className=' bg-[#47d147] text-black w-screen h-screen font-a'>
-      <div className='flex justify-start absolute inset-y-52 w-[790px] h-[750px] inset-x-4' style={{ opacity: 0.2 }}>
-      <Image src="/1.png" layout="fill" objectFit="cover" />
+      <div className='flex justify-start absolute inset-y-22 w-full h-full inset-x-0' style={{ opacity: 0.2 }}>
+      <Image src="/bg.gif" layout="fill" objectFit="cover" />
       </div>
 
       <div className="relative z-10 pt-8 flex flex-col items-center">
