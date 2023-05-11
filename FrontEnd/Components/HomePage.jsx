09944e526c68027f6c2dd6890d0ce83f327359ed
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-11 06:21:26 +0545

    homepage to mobile responsive

--- FrontEnd/Components/HomePage.jsx
+++ FrontEnd/Components/HomePage.jsx
@@ -13,31 +13,34 @@ const HomePage = () => {
       <Image src="/sadd.gif" layout="fill" objectFit="cover" alt="Sed Pepe" priority={true}/>
       </div>
 
-      <div className="relative z-10 pt-8 flex flex-col items-center">
-        <div className=' gap-8 flex flex-col'>
-        <h1 className='font-semibold text-5xl text-center text-white underline-offset-8 underline cursor-pointer'>SED PEPE</h1>
+      <div className="relative z-10 pt-8 flex flex-col items-center justify-center">
+        <div className=' gap-4 flex flex-col'>
+        <h1 className='font-semibold text-5xl text-center text-white cursor-pointer'>SED PEPE</h1>
         
-        <div className='flex items-center mx-64 pb-8 cursor-pointer'>  
-          <Image src='/lgo.png' width={400} height={400} alt="Sed Pepe"/>
+        <div className='flex items-center pb-1 md:pt-8 pt-2 cursor-pointer justify-center'>  
+       
+          <Image src='/lgo.png' height={300} width={300} alt="Sed Pepe"/>
+        
         </div>
         
-        <div className='w-[60rem]'>
-        <p className=' font-medium text-2xl text-center text-white'> 
+        <div className='w-11/12 flex flex-col justify-center text-center items-center md:w-[60rem]'>
+          <div className='w-11/12 flex flex-col text-center items-center pt-2 md:pt-8'>
+        <p className=' font-medium text-lg text-center text-white pl-6 md:pb-8'> 
         Pepe has become Sad of all the Atrocities of Humanity and Pepe is on the Brink of Devastation. Pepe is 
         very fed up of the World and its ways and is here to change that ! He wants the War to stop ASAP and will help in 
         all possible ways to bring Peace to the world in whatever small way possible.
         </p>
         </div>
         </div>
+        </div>
 
-        <div className=' w-screen h-[22rem] flex justify-center pt-12'>
-          <div className='bg-[#268426] w-[80rem] flex flex-col rounded-2xl border-2 drop-shadow-lg text-center pt-4'>
-            <h3 className="font-a text-white text-2xl underline pb-4">About</h3>
-            <div className='flex justify-center'>
-              <p className='font-a text-white text-lg w-[55rem]'>
+        <div className=' w-11/12 flex justify-center pt-2 md:pt-12 md:w-screen '>
+          <div className='bg-[#268426] w-11/12 md:w-[80rem] h-[25.5rem] md:h[22rem] flex flex-col rounded-2xl border-2 drop-shadow-lg text-center pt-4'>
+            <h3 className="font-a text-white text-2xl underline pb-2 md:pt-8 md:pb-4">About</h3>
+            <div className='flex justify-center p-2'>
+              <p className='font-a text-white text-lg w-[55rem] pb-4 md:pt-8'>
               Sed Pepe $SPEPE is a Meme Coin Project on Ethereum L2 Solution
-              Arbitrum. Pepe After Witnessing all the Violence , Hatred , Greed and Atrocities Committed by Humanity has 
-              become extremely Sad with the Humans. He has now come in the form of a Deflationary ERC-20 Token with a 4% tax on Buy/Sell that donates 
+              Arbitrum. He has now come in the form of a Deflationary ERC-20 Token with a 4% tax on Buy/Sell that donates 
               a Percentage of each Buy & Sell for Humanitarian Causes. He also Boasts a Strong Deflationary Mechanism That Burns
               1.5% of each Buy/Sell of $SPEPE. Not Only have he come with a Noble Cause but also with UTILITY unlike other PEPE Versions, 
               $SPEPE will feature a Full Ecosystem down the line that is revolved Around the token including but not limited to De-Fi , NFTs 
--- FrontEnd/Components/NavBarComponent.jsx
+++ FrontEnd/Components/NavBarComponent.jsx
@@ -24,20 +24,20 @@ const NavBarComponent = () => {
     }, []);
 
   return (
-    <div className='flex flex-row justify-between bg-[#2cb52c] items-center h-20 gap-8 w-screen'>
+    <div className='flex flex-row justify-start bg-[#2cb52c] items-center h-20 w-screen pl-2 pr-2 md:pl-16 md:justify-between '>
         <Link href={"./"}>
-        <div className='flex w-80 rounded-2xl justify-center pl-16 pt-2 pb-2 items-center gap-4 cursor-pointer'>
-          <Image src="/lgo.png" width={80} height={80} alt="Sed Pepe Logo"/>
-          <h4 className=' text-3xl font-a text-white font-semibold'> SED PEPE</h4>
+        <div className='flex w-1/3 rounded-2xl justify-start pt-2 pb-2 items-center cursor-pointer gap-2 md:w-80'>
+          <Image src="/lgo.png" width={60} height={60} alt="Sed Pepe Logo"/>
+          <h4 className=' text-m font-a text-white font-semibold md:text-3xl'> $SPEPE</h4>
         </div>
         </Link>
 
-      <div className='flex flex-row w-4/6 items-center justify-start gap-4 text-white text-xl font-bold font-a'>
+      <div className='flex flex-row w-2/3 items-center text-center justify-start gap-1 text-white text-sm md:text-xl font-bold font-a md:w-4/6 md:gap-4'>
         <Link href ={'./'}><div className='flex rounded-xl p-2 hover:text-red-500 cursor-pointer hover:scale-110'>HOME</div></Link>
-        <Link href ={'./litepaper'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>LITEPAPER</div></Link>
-        <Link href ={'./'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>ROADMAP</div></Link>
-        <Link href ={'./mint-id'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>MINT IDs</div></Link>
-        <Link href ={'./buypepe'}><div className='flex rounded-xl p-2  hover:text-red-500 cursor-pointer hover:scale-110'>BUY $SPEPE</div></Link>
+        <Link href ={'./litepaper'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>LITEPAPER</div></Link>
+        <Link href ={'./'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>ROADMAP</div></Link>
+        <Link href ={'./mint-id'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>MINT IDs</div></Link>
+        <Link href ={'./buypepe'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>BUY $SPEPE</div></Link>
       </div>
       
     </div>
