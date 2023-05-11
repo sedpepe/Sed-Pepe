9ea7dba6729138d758fb254345a4af16ddd4c373
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-11 06:34:47 +0545

    Update Footer.jsx

--- FrontEnd/Components/Footer.jsx
+++ FrontEnd/Components/Footer.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 const Footer = () => {
   return (
-    <div className='flex bg-[#2cb52c] items-center h-16 gap-24 w-screen justify-center'>
-          <div className='flex gap-8 items-center justify-start pr-4 pl-4 bg-[#3aac3a] p-1 rounded-3xl drop-shadow-lg h-12'>
-          <h2 className='font-semibold text-lg text-white'>Socials:</h2>
+    <div className='flex bg-[#2cb52c] items-center h-16 gap-2 md:gap-24 max-w-screen justify-center'>
+          <div className='flex gap-2 md:gap-8 items-center justify-start pr-4 pl-4 bg-[#3aac3a] p-1 rounded-3xl drop-shadow-lg h-12 w-1/2 md:w-1/6'>
+          <h2 className='font-semibold text-m text-white'>Socials:</h2>
           <div className=' flex pr-1 gap-4'>
             <Link href={"https://twitter.com/pepe_sed"}><Image src='/twitter.png' width={40} height={40} className="pt-2" alt="twitter"/></Link>
             <Link href={"https://t.me/sedpepe_chat"}><Image src='/tg2.png' width={40} height={40} className="pt-2" alt="telegram"/></Link>
@@ -14,10 +14,12 @@ const Footer = () => {
           </div>
           </div>
 
-          <div className='flex gap-8 items-center justify-end pl-4 pr-4 bg-[#3aac3a] p-1 rounded-3xl drop-shadow-lg h-12'>
-          <h4 className='font-semibold text-lg  text-white'>Powered By :</h4>
+          <div className='flex gap-2 md:gap-8 items-center pl-4 pr-4 bg-[#3aac3a] p-1 rounded-3xl drop-shadow-lg h-12 w-1/2 md:w-1/6 justify-between'>
+          <div className=' flex font-semibold text-m text-white gap-1'> <p>Powered</p><p>by:</p></div>
+          <div className='flex items-center gap-2 md:gap-8'>
           <Link href={"./"}><Image src='/therdWeed.svg' width={35} height={35} className="pt-2"/></Link>
           <Link href={"./"}><Image src='/Arbitrum.png' width={55} height={55} className="pt-2"/></Link>
+          </div>
           </div>
     </div>
   )
