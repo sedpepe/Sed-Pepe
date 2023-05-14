9f30965af17b702491691f31dee2d6d49bc6b56e
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-15 02:01:56 +0545

    Update PPlib.sol

--- Backend/contracts/BETA/libraries/PPlib.sol
+++ Backend/contracts/BETA/libraries/PPlib.sol
@@ -9,6 +9,9 @@ library PPlib {
         string ProfilePicURI;
         string gender;
         address Web3Wallet;
+        uint256 postCount; //controlled by blogController
+        uint256 friendCount; //controlled by blogController
+        uint256 followerCount; //controlled by blogController
         bool isBlacklistedUser;
     }
     struct Value{
@@ -16,4 +19,37 @@ library PPlib {
         uint256 totalDeposited;
         uint256 totalWithdrawn;
     }
+    //mapping TokenId => Struct Posts 
+    struct Posts{
+        string ImageUri;
+        string Title;
+        string Subject;
+        string Body;
+        uint256 timestamp;
+        uint256 likes;
+        string reactions;
+        bytes32 PostHashId;
+        bool isDeleted;
+    }
+    //commentSection mapping PostHash => CommenterNftId => Comment struct
+    struct Comment{
+        string comment;
+        bytes32 commentHashId;
+        bool isDeleted;
+        string reactions;
+        uint256 likes;
+    }
+    //mapping tokenId => (tokenId => Struct Friend)
+    struct Friend{
+        bool isFriendOftokenId;
+        uint256 timestampofAdd;
+        uint256 timestampOfRemove; 
+    }
+    //mapping tokenId => (tokenId => Struct Friend)
+    struct Follower{
+        bool isFollowedToId;
+        uint256 timestampofAdd;
+        uint256 timestampOfRemove; 
+    }
+
 }
