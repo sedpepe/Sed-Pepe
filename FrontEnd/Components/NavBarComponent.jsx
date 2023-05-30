91f1a403f176c805008a625e38cee9e654e6e86d
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-31 00:11:58 +0545

    Update NavBarComponent.jsx

--- FrontEnd/Components/NavBarComponent.jsx
+++ FrontEnd/Components/NavBarComponent.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { AppContext } from '../Api/contextApi';
 import { useContext } from 'react';
-import LoginPage from './LoginPage';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const NavBarComponent = () => {
     const {connectedUser} =useContext(AppContext);
-
+    const [openMenu , setOpenMenu] = useState(false);
   return (
     <div className='flex flex-row justify-between bg-[#2cb52c] items-center h-16 min-w-screen pl-4 pr-2 md:pl-6 md:justify-between gap-4 border-[#2b892b] border-2 '>
         <div className=' flex min-w-1/2 rounded-2xl justify-start pt-2 pb-2 items-center cursor-pointer gap-7 text-2xl font-bold'>
@@ -19,9 +18,18 @@ const NavBarComponent = () => {
         
 
       <div className='flex  flex-row w-2/3 items-center justify-end pr-1 text-center text-white text-xs md:text-xl font-bold font-a md:w-4/6 md:gap-4'>
-        <button onClick={()=> window.location.reload()}><Image src="/menuicon.svg" width={40} height={40}/></button>
+        <button onClick={()=> setOpenMenu(true)}><Image src="/menuicon.svg" width={40} height={40}/></button>
       </div>
       
+
+      {openMenu && (
+        <div className="fixed inset-0 bg-[#187818] flex justify-center items-center z-40">
+          <div className='flex flex-col items-center gap-20 font-semibold italic'>
+            <h1 className='text-5xl underline-offset-4 underline text-gray-300'>CONTENTS</h1>
+            <button className='bg-red-600 rounded-full w-24 h-24 text-blue-300' onClick={()=> setOpenMenu(false)}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
