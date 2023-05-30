df8731c01c4a7d53c836e4dd5e032bef40376157
Bishal Kr Bhuyan <94212983+BOBseal@users.noreply.github.com>
2023-05-30 23:42:27 +0545

    responsive fixing

--- FrontEnd/Components/HomePage.jsx
+++ FrontEnd/Components/HomePage.jsx
@@ -8,15 +8,16 @@ import Link from 'next/link';
 const HomePage = () => {
     const {connectedUser ,MintNFT } =useContext(AppContext);
   return (
-    <div className=' bg-[#2b892b] w-screen min-h-screen font-a text-white'>
+    <div className=' bg-[#2b892b] min-w-screen min-h-screen font-a text-white'>
+      
       <div className='flex justify-start absolute inset-y-22 w-screen min-h-screen inset-x-0 ' style={{ opacity: 0.18 }}>
       <Image src="/sadd.gif" layout="fill" objectFit='cover' alt="Sed Pepe" priority={true}/>
       </div>
 
       <div className='main'>
        
-       <div className='flex justify-center pb-2'> {/* HEAD INTRO*/}
-        <div className='title flex pt-4 flex-col justify-center w-10/12 text-center gap-2'>
+       <div className='flex justify-center pb-2 md:pt-8 lg:pt-10'> {/* HEAD INTRO*/}
+        <div className='title flex pt-4 flex-col justify-center w-10/12 md:w-5/12 lg:w-3/12 text-center gap-2'>
           <h1 className='text-5xl justify-center flex bg-[#247224] border drop-shadow-xl p-2 rounded-full'>$SED PEPE</h1>
           <div className='flex justify-center opacity-100 pt-6'>
             <Image src="/SPEPENFTs.gif" height={300} width={300} alt="Sad" className='absolute rounded-2xl border border-black drop-shadow-xl'/>
@@ -25,8 +26,8 @@ const HomePage = () => {
         </div>
         </div>
         
-        <div className='flex p-6 w-screen justify-center'>
-          <div className=' bg-[#247224] border drop-shadow-lg w-11/12 flex text-center justify-center flex-col items-center pt-4 pb-4 pl-2 pr-2'>
+        <div className='flex p-6 w-full justify-center'>
+          <div className=' bg-[#247224] border drop-shadow-lg w-11/12 md:w-8/12 lg:w-5/12 flex text-center justify-center flex-col items-center pt-4 pb-4 pl-2 pr-2'>
           <h1 className='text-2xl pb-4'>ABOUT SED PEPE</h1>
           
           <p>Sed Pepe is an ERC20 Meme Coin that Has Actual Utility and A fully Decentralized Ecosystem Built Around it. Pepe Thinks It is Very Sed to Not have Utility
--- FrontEnd/Components/NavBarComponent.jsx
+++ FrontEnd/Components/NavBarComponent.jsx
@@ -9,22 +9,16 @@ const NavBarComponent = () => {
     const {connectedUser} =useContext(AppContext);
 
   return (
-    <div className='flex flex-row justify-start bg-[#2cb52c] items-center h-16 w-screen pl-4 pr-2 md:pl-16 md:justify-between gap-4 border-[#2b892b] border-2 '>
+    <div className='flex flex-row justify-between bg-[#2cb52c] items-center h-16 min-w-screen pl-4 pr-2 md:pl-6 md:justify-between gap-4 border-[#2b892b] border-2 '>
         <Link href={"./"}>
-        <div className='flex w-1/3 rounded-2xl justify-start pt-2 pb-2 items-center cursor-pointer gap-1 md:w-80'>
-          <div className='absolute w-8 h-8'>
-            <Image src="/lgo.png"  layout="fill" objectFit="cover" alt="Sed Pepe Logo"/>
-          </div>
-          <h4 className=' text-sm font-a text-white font-semibold md:text-3xl pl-10 underline underline-offset-1'> $SPEPE</h4>
+        <div className=' flex max-w-1/4 rounded-2xl justify-start pt-2 pb-2 items-center cursor-pointer gap-4 md:gap-7 md:w-80'>
+            <Image src="/lgo.png"  height={47} width={47} alt="Sed Pepe Logo"/>
+            <p className='text-white'>SEDPEPE</p>
         </div>
         </Link>
 
-      <div className='flex flex-row w-2/3 items-center text-center justify-start text-white text-xs md:text-xl font-bold font-a md:w-4/6 md:gap-4'>
-        <Link href ={'./'}><div className='flex rounded-xl p-1 hover:text-red-500 cursor-pointer hover:scale-110'>HOME</div></Link>
-        <Link href ={'./docs'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>DOCS</div></Link>
-        <Link href ={'./roadmap'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>ROADMAP</div></Link>
-        <Link href ={'./presale'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>PRESALE</div></Link>
-        <Link href ={'./support'}><div className='flex rounded-xl p-1  hover:text-red-500 cursor-pointer hover:scale-110'>SUPPORT</div></Link>
+      <div className='flex  flex-row w-2/3 items-center justify-end pr-1 text-center text-white text-xs md:text-xl font-bold font-a md:w-4/6 md:gap-4'>
+        <button onClick={()=> openModel(true)}><Image src="/menuicon.svg" width={40} height={40}/></button>
       </div>
       
     </div>
